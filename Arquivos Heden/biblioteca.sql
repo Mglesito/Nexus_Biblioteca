eee9cea2ba99e6434d76b0b808539804cc97896e
Mglesito <80413257+Mglesito@users.noreply.github.com>
2026-08-13 18:36:41 -0300

    Add files via upload

---
+++ Arquivos Heden/biblioteca.sql
@@ -0,0 +1,234 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13/08/2026 às 23:34
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `aluno`
+--
+
+CREATE TABLE `aluno` (
+  `cpf` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `ano` int(11) NOT NULL,
+  `turma` varchar(50) NOT NULL,
+  `curso` varchar(100) NOT NULL,
+  `endereco` varchar(200) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `contato` varchar(20) NOT NULL,
+  `data_entrada` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `bibliotecario`
+--
+
+CREATE TABLE `bibliotecario` (
+  `cpf` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `emprestimo`
+--
+
+CREATE TABLE `emprestimo` (
+  `id` int(11) NOT NULL,
+  `data_emprestimo` date NOT NULL,
+  `data_devolucao` date DEFAULT NULL,
+  `devolvido` tinyint(1) NOT NULL,
+  `cpf` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `livro`
+--
+
+CREATE TABLE `livro` (
+  `id` int(11) NOT NULL,
+  `registro` int(11) DEFAULT NULL,
+  `autor` varchar(150) NOT NULL,
+  `titulo` varchar(200) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `suporte`
+--
+
+CREATE TABLE `suporte` (
+  `cpf` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tombo`
+--
+
+CREATE TABLE `tombo` (
+  `registro` int(11) NOT NULL,
+  `data_entrada` datetime NOT NULL,
+  `autor` varchar(150) NOT NULL,
+  `titulo` varchar(200) NOT NULL,
+  `volume` int(11) NOT NULL,
+  `exemplar` int(11) NOT NULL,
+  `edicao` int(11) NOT NULL,
+  `ano` year(4) NOT NULL,
+  `local` varchar(100) NOT NULL,
+  `tipo_aquisicao` varchar(100) NOT NULL,
+  `codigo_genero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `cpf` int(11) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuario`
+--
+
+INSERT INTO `usuario` (`cpf`, `email`, `senha`, `tipo_usuario`) VALUES
+(123456, 'bla@gmail.com', '1234', 1),
+(12345678, 'email@email.com', '12345', 2);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices de tabela `bibliotecario`
+--
+ALTER TABLE `bibliotecario`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices de tabela `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cpf` (`cpf`);
+
+--
+-- Índices de tabela `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `registro` (`registro`);
+
+--
+-- Índices de tabela `suporte`
+--
+ALTER TABLE `suporte`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices de tabela `tombo`
+--
+ALTER TABLE `tombo`
+  ADD PRIMARY KEY (`registro`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `livro`
+--
+ALTER TABLE `livro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `aluno`
+--
+ALTER TABLE `aluno`
+  ADD CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuario` (`cpf`);
+
+--
+-- Restrições para tabelas `bibliotecario`
+--
+ALTER TABLE `bibliotecario`
+  ADD CONSTRAINT `bibliotecario_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuario` (`cpf`);
+
+--
+-- Restrições para tabelas `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  ADD CONSTRAINT `emprestimo_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `aluno` (`cpf`);
+
+--
+-- Restrições para tabelas `livro`
+--
+ALTER TABLE `livro`
+  ADD CONSTRAINT `livro_ibfk_1` FOREIGN KEY (`registro`) REFERENCES `tombo` (`registro`);
+
+--
+-- Restrições para tabelas `suporte`
+--
+ALTER TABLE `suporte`
+  ADD CONSTRAINT `suporte_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuario` (`cpf`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
